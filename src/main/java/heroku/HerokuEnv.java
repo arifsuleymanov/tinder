a19d54f3eaa260c6cc802bef5c9bd69434daafa0
ARIF SULEYMANOV <86264694+arifsuleymanov@users.noreply.github.com>
2022-11-03 00:18:02 +0400

    Update HerokuEnv.java

--- src/main/java/heroku/HerokuEnv.java
+++ src/main/java/heroku/HerokuEnv.java
@@ -11,20 +11,14 @@ public class HerokuEnv {
     }
 
     public static String jdbc_url() {
-        String url = System.getenv("JDBC_DATABASE_URL");
-        if (url == null) throw new IllegalArgumentException("JDBC_DATABASE_URL is empty!!!");
-        return url;
+        return "jdbc:postgresql://ec2-3-220-207-90.compute-1.amazonaws.com:5432/dgoetfuaviiqk";
     }
 
     public static String jdbc_username() {
-        String url = System.getenv("JDBC_DATABASE_USERNAME");
-        if (url == null) throw new IllegalArgumentException("JDBC_DATABASE_USERNAME is empty!!!");
-        return url;
+        return "fnerjqwfyojtcl";
     }
 
     public static String jdbc_password() {
-        String url = System.getenv("JDBC_DATABASE_PASSWORD");
-        if (url == null) throw new IllegalArgumentException("JDBC_DATABASE_PASSWORD is empty!!!");
-        return url;
+        return "be7c163971b7a147673a5e14a6c2e842ab706e50e1e209eb9cb3662d41716d80";
     }
 }
